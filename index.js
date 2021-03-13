da01c94ab5b4777755452f9a5fd7d3192f499481
zaidmukaddam <76097144+zaidmukaddam@users.noreply.github.com>
2021-03-13 18:43:48 +0530

    Create Climate Change Page 2

---
+++ index.js
@@ -0,0 +1,6 @@
+function AlertIt() {
+var answer = confirm ("Please click on OK to continue.")
+if (answer)
+window.location="https://www.google.com"; // Add the Git link
+}
+window.onload = alert("It's nice to see you again! " + localStorage.getItem("storageName")+"!");
