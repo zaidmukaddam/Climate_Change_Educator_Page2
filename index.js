0894974ef4550f6d9586a400c16ce94f638d5cb2
zaidmukaddam <76097144+zaidmukaddam@users.noreply.github.com>
2021-03-14 18:23:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function AlertIt() {
-var answer = confirm ("Please click on OK to continue.")
+var answer = confirm ("Please click on OK to continue to Page 2.")
 if (answer)
-window.location="https://zaidmukaddam.github.io/Climate_Change_Educator_Page1/"; // Add the Git link
+window.location="https://zaidmukaddam.github.io/Climate_Change_Educator_Page1/"; 
 }
 window.onload = alert("It's nice to see you again! " + localStorage.getItem("storageName")+"!");
